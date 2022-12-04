139155a3634a387e7a392c6e065487841d4abcc0
Pisarev Danila <danila.pisarev@payture.com>
2022-12-04 11:26:12 +0300

    refactor: change env and add route to activate

--- server/routes/auth.router.js
+++ server/routes/auth.router.js
@@ -5,6 +5,7 @@ const { authController } = require("../controllers");
 const router = express.Router();
 
 router.post("/registration", authController.registration);
+router.get("/activate/:link", authController.activate);
 router.post("/login", authController.login);
 router.post("/logout", authController.logout);
 router.post("/refresh", authController.refresh);
--- server/services/mail.service.js
+++ server/services/mail.service.js
@@ -16,7 +16,7 @@ class MailService {
     this.transporter.sendMail({
       from: process.env.SMTP_USER,
       to,
-      subject: "Account activation on " + process.env.APP_URL,
+      subject: "Account activation on " + process.env.SERVER_URL,
       html: `<div>
         <h1>To activate account click to the link</h1>
         <a href="${link}" target="_black" rel="noreferrer">${link}</a>
