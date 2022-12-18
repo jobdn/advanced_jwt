919e2c103e1500b2cbde1aca7d086b98e4767b1b
Pisarev Danila <danila.pisarev@payture.com>
2022-12-18 23:02:23 +0300

    feat: add interceptor for response

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -1,11 +1,12 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import { AxiosRequestConfig, RawAxiosRequestHeaders } from "axios";
 
 import { AvailableToken } from "models/http";
-import { APi_URL } from "constants/http.constants";
+import { AuthResponse } from "models/response/AuthResponse";
+import { API_URL } from "constants/http.constants";
 
 const backend = axios.create({
-  baseURL: APi_URL,
+  baseURL: API_URL,
   withCredentials: true,
 });
 
@@ -16,5 +17,34 @@ backend.interceptors.request.use((config: AxiosRequestConfig) => {
   )}`;
   return config;
 });
+
+backend.interceptors.response.use(
+  (config: AxiosResponse) => {
+    return config;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (
+      error.response.status === 401 &&
+      error.config &&
+      !originalRequest._isRetry
+    ) {
+      originalRequest._isRetry = true;
+      try {
+        const response = await axios.get<AuthResponse>(
+          API_URL + "/auth/refresh",
+          {
+            withCredentials: true,
+          }
+        );
+        localStorage.setItem(AvailableToken.ACCESS, response.data.accessToken);
+        return backend.request(originalRequest);
+      } catch (error) {}
+    }
+
+    throw error;
+  }
+);
 
 export default backend;
